b1a7f0aa1c84baafac8b431865551779df0e8231
277896015 <g277896015@gmail.com>
2026-03-08 07:07:52 +0800

    Update tg.py

--- tg.py
+++ tg.py
@@ -1,18 +1,26 @@
 import os
-from telegram import Bot
+import requests
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 
-bot = Bot(token=BOT_TOKEN)
+URL = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
 
 def send(msg):
 
     try:
-        bot.send_message(
-            chat_id=CHAT_ID,
-            text=msg,
-            disable_web_page_preview=True
+
+        requests.post(
+            URL,
+            json={
+                "chat_id": CHAT_ID,
+                "text": msg,
+                "disable_web_page_preview": True
+            },
+            timeout=10
         )
+
     except Exception as e:
+
         print("TG error:", e)
