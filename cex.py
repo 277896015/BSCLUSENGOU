4698cee6440650127aea6ba824bfd88a6527b434
277896015 <g277896015@gmail.com>
2026-03-08 22:54:27 +0800

    Create cex.py

---
+++ cex.py
@@ -0,0 +1,20 @@
+CEX = {
+
+# Binance
+"0x3f5ce5fbfe3e9af3971dd833d26ba9b5c936f0be": "Binance",
+"0x28c6c06298d514db089934071355e5743bf21d60": "Binance",
+
+# KuCoin
+"0x8894e0a0c962cb723c1976a4421c95949be2d4e3": "KuCoin",
+"0x635308e731a878741bfec299e67f5fd28c7553d9": "KuCoin",
+
+# OKX
+"0x21a31ee1afc51d94c2efccaa2092ad1028285549": "OKX",
+
+# Gate
+"0x0d0707963952f2fba59dd06f2b425ace40b492fe": "Gate",
+
+# MEXC
+"0x4e9ce36e442e55ecd9025b9a6e0d88485d628a67": "MEXC",
+
+}
