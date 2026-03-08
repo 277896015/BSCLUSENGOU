e27f173b78e297fdebc3f2ec3ea0239042d4b444
277896015 <g277896015@gmail.com>
2026-03-08 23:06:36 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -3,15 +3,15 @@ from web3 import Web3
 KUCOIN = "0x689c56aef474df92d44a1b70850f808488f9769c"
 
 def get_token_name(w3, token):
-
-    abi = [{
-        "name":"name",
-        "outputs":[{"type":"string"}],
-        "inputs":[],
-        "stateMutability":"view",
-        "type":"function"
-    }]
-
+    abi = [
+        {
+            "name": "name",
+            "outputs": [{"type": "string"}],
+            "inputs": [],
+            "stateMutability": "view",
+            "type": "function"
+        }
+    ]
     try:
         c = w3.eth.contract(address=token, abi=abi)
         return c.functions.name().call()
@@ -19,16 +19,8 @@ def get_token_name(w3, token):
         return "Unknown"
 
 def get_bnb_balance(w3, addr):
-
     try:
         balance = w3.eth.get_balance(addr)
         return w3.from_wei(balance, "ether")
     except:
-        return 0
-
-def is_kucoin(addr):
-
-    if not addr:
-        return False
-
-    return addr.lower() == KUCOIN.lower()
+        return 0.0
